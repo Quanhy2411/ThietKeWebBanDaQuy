331b662715d4ce451475de0f415c73a3153ef861
Quanhy2411 <teaplus2k6@gmail.com>
2026-03-30 20:17:46 +0700

    upadate code

--- assets/js/home.js
+++ assets/js/home.js
@@ -15,3 +15,49 @@ var swiper = new Swiper(".mySlider", {
     prevEl: ".swiper-button-prev",
   },
 });
+var swiper2 = new Swiper(".dsSpDaXem", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+
+  navigation: {
+    nextEl: ".dsSpDaXem .swiper-button-next",
+    prevEl: ".dsSpDaXem .swiper-button-prev",
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+var brandSwiper = new Swiper(".brandSlider", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  loop: true,
+
+  navigation: {
+    nextEl: ".brandSlider .swiper-button-next",
+    prevEl: ".brandSlider .swiper-button-prev",
+  },
+
+  pagination: {
+    el: ".brandSlider .swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
