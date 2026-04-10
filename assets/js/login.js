8409c1615f7a2033735d66bfe03c07c9a133fb6a
Quanhy2411 <teaplus2k6@gmail.com>
2026-04-11 03:05:44 +0700

    update

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,6 @@
+const loginLink = document.getElementById("loginLink");
+const logoutLink = document.getElementById("logoutLink");
+
 function checkLoginStatus() {
   if (sessionStorage.getItem("loggedIn") === "true") {
     loginLink.style.display = "none";
@@ -8,15 +11,24 @@ function checkLoginStatus() {
   }
 }
 
-function logout() {
-  sessionStorage.removeItem("loggedIn");
-  alert("Bạn đã đăng xuất!");
-  window.location.href = "page/register.html";
-}
-
 logoutLink.addEventListener("click", function (e) {
   e.preventDefault();
-  logout();
+  sessionStorage.removeItem("loggedIn");
+  alert("Bạn đã đăng xuất!");
+  window.location.href = "page/register.html"; // hoặc quay về login.html
 });
 
 checkLoginStatus();
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  if (user === "admin" && pass === "123456") {
+    sessionStorage.setItem("loggedIn", "true");
+    alert("Đăng nhập thành công!");
+    window.location.href = "../index.html"; // quay về trang chủ
+  } else {
+    alert("Sai tài khoản hoặc mật khẩu!");
+  }
+});
